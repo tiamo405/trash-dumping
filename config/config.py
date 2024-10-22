40e97843efd9327fbd1090d606c236a0f557b775
tiamo.405 <nam05052002@gmail.com>
2024-10-22 23:35:32 +0700

    refactor ip, add debug

--- config/config.py
+++ config/config.py
@@ -1,14 +1,17 @@
 # minio-config
-ENDPOINT ="192.168.5.80:9000"
+ENDPOINT ="192.168.5.106:9000"
 ACCESS_KEY="minioadmin"
 SECRET_KEY="minioadmin"
 BUCKET="trash-dumping"
 SECURE = False
 
 # model
-WEIGHT = 'checkpoints/ava/yowo_v2_large_ava_k32.pth'
+# WEIGHT = 'checkpoints/ava/yowo_v2_large_ava_k32.pth'
+WEIGHT = 'checkpoints/trash/yowo_v2_medium/yowo_v2_medium_epoch_50.pth'
 
 # video
 VIDEO_PATH = 'test_model/video_test/2.mp4' # or camera
 
+# debug
+DEBUG = True
 
