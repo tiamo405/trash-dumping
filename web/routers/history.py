1bde2b9656225704dd7a9c7d4de43ee464ace5af
tiamo.405 <nam05052002@gmail.com>
2024-11-04 15:23:23 +0700

    fix

--- web/routers/history.py
+++ web/routers/history.py
@@ -40,25 +40,30 @@ async def get_history_video(id_image_violation: str , page: int = 1, limit: int 
     # get all video in date camera
     all_videos = list(mongo_manager.collection_video.find({"camera_id": camera_id, "date_time": date_violation}))
     # phan trang
-    start_index = (page - 1) * limit
-    end_index = start_index + limit
-    paginated_data = all_videos[start_index:end_index]
-    
     video_violations = []
-    for video in paginated_data:
+    for video in all_videos:
         start_time = video["start_time"]
         end_time = video["end_time"]
         if detect_timestamp >= start_time and detect_timestamp <= end_time:
             video["_id"] = str(video["_id"])
             video["url_video"] = s3.geturl(video["video_path"])
             video_violations.append(video)
+    start_index = (page - 1) * limit
+    end_index = start_index + limit
+    paginated_data = video_violations[start_index:end_index]
     totalPage = len(video_violations) // limit + 1
-    return Response(200, entities={"videos": video_violations, "page": page, "limit": limit, "totalPage": totalPage})
+    return Response(200, entities={"videos": paginated_data, "page": page, "limit": limit, "totalPage": totalPage})
 
 # get all violation has is_violation = True
 @view_router.get("/get_all_violation")
-async def get_all_violation(page: int = 1, limit: int = 10):
-    violation_images = list(mongo_manager.collection_violation_image.find({"is_violation": "True"}))
+async def get_all_violation(location : str = None, is_violation : bool = True , page: int = 1, limit: int = 10):
+    if location is None or location == "":
+        violation_images = list(mongo_manager.collection_violation_image.find({"is_violation": is_violation}))
+    else:
+        cams = mongo_manager.collection_camera.find({"location": location})
+        for cam in cams:
+            camera_id = str(cam["_id"])
+            violation_images = list(mongo_manager.collection_violation_image.find({"camera_id": camera_id, "is_violation": is_violation}))
     # phan trang
     start_index = (page - 1) * limit
     end_index = start_index + limit
@@ -73,16 +78,19 @@ async def get_all_violation(page: int = 1, limit: int = 10):
 
 # set is_violation = True or False
 @view_router.post("/set_violation")
-async def set_violation(id_image: str , is_violation: str):
+async def set_violation(id_image: str , is_violation: bool):
     violation = mongo_manager.collection_violation_image.find_one({"_id": ObjectId(id_image)})
     violation["is_violation"] = is_violation
     mongo_manager.collection_violation_image.update_one({"_id": violation["_id"]}, {"$set": violation})
     return Response(200, entities={"id_image": id_image, "is_violation": "True"})
 
 # delete violation_image
-@view_router.post("/delete_violation")
+@view_router.delete("/delete_violation")
 async def delete_violation(id_image: str):
     violation = mongo_manager.collection_violation_image.find_one({"_id": ObjectId(id_image)})
+    if not violation:
+        return Response(1, error_resp=1)
     s3.delete_file(id_image + ".jpg")
     mongo_manager.collection_violation_image.delete_one({"_id": violation["_id"]})
     return Response(200, entities={"id_image": id_image})
+
