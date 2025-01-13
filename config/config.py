969af96d2d925adc12af9f5008deccc8ffb867f6
tiamo.405 <nam05052002@gmail.com>
2025-01-13 09:03:50 +0700

    done do an

--- config/config.py
+++ config/config.py
@@ -11,7 +11,7 @@ WEIGHT = 'checkpoints/trash/yowo_v2_medium/yowo_v2_medium_epoch_50.pth'
 
 # video
 VIDEO_PATH = 'trash/video/vlc-record-2024-12-19-09h48m58s-WIN_20241211_15_40_47_Pro.mp4-.mp4' # or camera
-# VIDEO_PATH = 'rtsp://cxview:gs252525@113.161.58.13:554/Streaming/Channels/701'
+
 
 # debug
 DEBUG = True
